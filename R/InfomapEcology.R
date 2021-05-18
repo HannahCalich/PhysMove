6f72f32f8ece3edb0ca2b9c5c5b4159320907a81
Hannah <hannah.calich@research.uwa.edu.au>
2021-05-18 14:13:19 +0800

    Testing Infomap

---
+++ R/InfomapEcology.R
@@ -0,0 +1,33 @@
+# Testing infomapecology package availble from: https://ecological-complexity-lab.githubio/infomap_ecology_package/
+library(infomapecology)
+
+# Sample monolayer directed network with hierarchical structure
+# Import data
+data("kongsfjorden_links")
+data("kongsfjorden_nodes")
+nodes <- kongsfjorden_nodes %>%
+  select(node_name=Species, node_id_original=NodeID, everything())
+
+interactions<- kongsfjorden_links %>%
+  select(from=consumer, to=resource) %>%
+  mutate_if(is.factor, as.character) %>%
+  mutate(weight=1)
+
+# Prepare network objects
+network_object <- create_monolayer_object(x=interactions, directed = T, bipartite = F, node_metadata = nodes)
+
+# Run infomap, allow hierarchical modules
+# Some species will have only incoming or outgoing links, so the next line will result in a warning
+infomap_object <- run_infomap_monolayer(network_object, infomap_executable='Infomap',
+                                        flow_model = 'directed',
+                                        silent=T,trials=100, two_level=F, seed=123)
+
+
+
+
+linklist<-read.delim("H:/My Documents/1_MyDocuments/PhD_2021/PhD/Methods Paper/R/Infomap_test/Bull_Probabilities20200210.txt")
+names(linklist)<-c("source", "target", "weight")
+
+test<-create_monolayer_object(linklist, directed = TRUE, bipartite=FALSE)
+
+
