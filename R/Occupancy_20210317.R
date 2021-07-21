1206a50cd89a6f71a6ec3a4bd09c6f4bdec53cef
Hannah <hannah.calich@research.uwa.edu.au>
2021-07-21 09:45:55 +0800

    Updated bw code in plotPDF and added a loop to find appropriate bw for scenarios when cell size is small.

--- R/Occupancy_20210317.R
+++ R/Occupancy_20210317.R
@@ -35,32 +35,32 @@ Occupancy<-function(species_df, gridcell=0.25, map=TRUE, colGrad=c("blue", "ligh
   #Loop through each location's coordinates and store counts when they occur in each cell (the cell numbers are exactly the same as python)
   Presence <- rep(0, totalcells)
   for (i in 1:dim(species_df)[1]){
-    coordlong <- floor(grid * (species_df[i,2] - longmin)) #original
-    coordlat <- floor(grid * (species_df[i,3] - latmin)) #How many rows up do we go from the 0-360 baseline? If point in 0-360deg range this value will be 0
-    cellnum <- (coordlong + grid * (longmax - longmin) * coordlat)+1 #Otherwise we have cell 0 and this makes the calculations incorrect below (0 was valid in Python but not in R)
+    coordlong <- floor(grid * (species_df[i,2] - longmin))
+    coordlat <- floor(grid * (species_df[i,3] - latmin))
+    cellnum <- (coordlong + grid * (longmax - longmin) * coordlat) + 1 # Need to add 1 here otherwise we have cell number 0, which was valid in Python but not in R.
     Presence[cellnum] <- Presence[cellnum] + 1
   }
 
-  ## Calculate the area of each grid cell with species were present by converting grid cell #s back to lat/lon then calculalting areas using spherical coordiantes
+  ## Calculate the area of each grid cell with species were present by converting grid cell numbers back to lat/lon then calculating areas using spherical coordinates
   AllOccup <- MyArea <- rep(0, totalcells)
   for(i in 1:length(Presence)){
     coordlat <- floor(i/(grid*(longmax-longmin)))
     ilat <- latmin + (coordlat / grid)
     MyArea[i] <- Radius^2 * abs(sin(rad * ilat) - sin(rad * (ilat + (1/grid)))) * (rad * (1/grid)) # Area for standard spherical coordinates in km^2 (same as Sphere_Cylindrical_Equal_Area projection in ArcGIS)
-    AllOccup[i] <- Presence[i]/MyArea[i] #This occupancy vector keeps the original cell number
+    AllOccup[i] <- Presence[i]/MyArea[i]
   }
   OccExp <- as.data.frame(cbind("CellNumber"=seq(1,totalcells,1),"Counts"=Presence,"Occupancy"=AllOccup, "Area"=MyArea, "Longitude"=c(0), "Latitude"=c(0)))
-  OccExp<-OccExp[which(OccExp$Occupancy != 0),]
+  OccExp <- OccExp[which(OccExp$Occupancy != 0),]
 
   for(i in 1:nrow(OccExp)){
-    coordlat <- floor(OccExp[i,1]/(grid*(longmax-longmin))) #Take origin cell number and divide by number of cells in longitude of grid.
+    coordlat <- floor(OccExp[i,1]/(grid*(longmax-longmin))) # Take origin cell number and divide by number of cells in longitude of grid.
     coordlong <- OccExp[i,1] - (grid*(longmax-longmin)) * coordlat
 
     if (coordlong==0) {
-      OccExp$Longitude[i]  <- (longmin + (360 / grid)) - 0.5*gridcell #midpoint of cell
+      OccExp$Longitude[i] <- (longmin + (360 / grid)) - 0.5*gridcell # midpoint of cell
       OccExp$Latitude[i] <- (latmin + (coordlat / grid)) - 0.5*gridcell
     } else {
-      OccExp$Longitude[i]  <- (longmin + (coordlong/grid)) - 0.5*gridcell
+      OccExp$Longitude[i] <- (longmin + (coordlong/grid)) - 0.5*gridcell
       OccExp$Latitude[i] <- (latmin + (coordlat / grid)) + 0.5*gridcell
     }
   }
@@ -77,30 +77,37 @@ Occupancy<-function(species_df, gridcell=0.25, map=TRUE, colGrad=c("blue", "ligh
       ggplot2::scale_fill_gradientn(colours = c(colGrad))
     tryCatch({
       z <- z +
-        ggplot2::borders("world", colour ="gray50", fill ="gray50", xlim = c(min(xyz$Longitude)- 0.5*gridcell, max(xyz$Longitude)+ 0.5*gridcell), ylim = c(min(xyz$Latitude)- 0.5*gridcell, max(xyz$Latitude)+ 0.5*gridcell))
+        ggplot2::borders("world", colour ="gray50", fill ="gray50", xlim = c(min(xyz$Longitude)-0.5*gridcell, max(xyz$Longitude)+0.5*gridcell), ylim = c(min(xyz$Latitude)- 0.5*gridcell, max(xyz$Latitude)+ 0.5*gridcell))
       }, error = function(e){message('Note: World polygon does not overlap with occupancy data')})
     plot(z)
   }
 
   if (pdfPlot==TRUE){
-    Occmin <- min(OccExp[which(OccExp$Occupancy!=0),3]) #what is the min occ value when occ > 0 (min occupancy of occupied cells)
-    bw <- log(max(OccExp$Occupancy)/Occmin)/log(nBins)
-    if (log(bw)<0){
-      message ("Ratio of occupancy values to number of bins has resulted in a bin width less than 1, which cannot be log transformed. Bin width has been changed to 1.1 from ", round(bw,2) )
-      bw <- 1.1
+    Occmin <- min(OccExp[which(OccExp$Occupancy!=0),3])
+    bw <- log(max(OccExp$Occupancy)/Occmin)/log(nBins) ## original
+    ### need to add an iterative loop that will change # breaks becaues after this runs bw can still be too small
+    if (log(bw)<1.2){
+      break_start <- 20
+      repeat {
+        h <- hist(log(OccExp$Occupancy), breaks=20)
+        bw <- exp(h$breaks[2]-h$breaks[1])
+        if (log(bw)>=1.2){
+          break
+        } else {
+          break_start <- break_start - 1
+        }
+      }
     }
     freq <- rep(0, nBins+1)
     for(i in 1:nrow(OccExp)){
       if (OccExp[i,3]>0){
-        b <- floor(log(OccExp[i,3]/Occmin)/log(bw) + 0.5) # ceiling is used here because if b = 0 it'll throw errors (python code uses int(#) but 0 is valid in python)
+        b <- floor(log(OccExp[i,3]/Occmin)/log(bw) + 0.5) #original
         freq[b] <- freq[b] + 1
       }
     }
     if (anyNA(freq)){
       stop ("Cell size too small to create pdf plot") # If users do not increase nBins to make up for a small cell size then the above code will results in NA and invalid values, which cause the code to crash.
     }
-    sumFreq <- sum(freq)
-    freq <- c(totalcells-sumFreq, freq) #pdf plot should consider all cells in work so instead of processing through empty cells I just add the empty ones to the beginning of this vector
     norm <- sum(freq)
     freq <- freq[freq>0]
     xs <- ys <- rep(0, length(freq))
@@ -112,8 +119,7 @@ Occupancy<-function(species_df, gridcell=0.25, map=TRUE, colGrad=c("blue", "ligh
     occplot <- data.frame(xs, ys)
     names(occplot) <- c("Occupancy(km^-2)","pdf")
     assign("occupancyPDFplot", occplot, envir = .GlobalEnv)
-    plot(xs, ys,log = "xy", xlim = c(Occmin/2,max(OccExp$Occupancy)*2), type = "l", col = "black", ylab = "pdf",xlab = expression('Occupancy (km'^'-2'*')'))
-    points(xs, ys, col = "black", pch = 19)
-
+    plot(xs, ys, log = "xy", type = "l", ylab = "pdf", xlab = expression('Occupancy (km'^'-2'*')'))
+    points(xs, ys, pch = 19)
     }
 }
