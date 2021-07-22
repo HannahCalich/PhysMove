0b852ef0f31a8696db20c33f1206310078b9225c
Hannah <hannah.calich@research.uwa.edu.au>
2021-07-22 18:36:02 +0800

    Corrected issues that happened when there were more communities than colours in the ggplot2 colour pallets (i.e., > 8 communities) by incorporating rColorBrewer. Removed land=TRUE and replaced with tryCatch function for plotting land borders instead.

--- R/CommunityMap.R
+++ R/CommunityMap.R
@@ -4,9 +4,7 @@
 #' To map only a subset of the communities, save the relevant level 1 community numbers to a vector and input as subset_communities.
 #' @param infomap_object Monolayer object that was output from the \code{\link{InfomapCommunities}} function.
 #' @param subset_communities Vector of communities to be plotted in map. Default is NULL.
-#' @param colPalette  Colour palette for map (applied to ggplot2::scale_fill_brewer). Default is "Dark2".
-#' @param land Add a land polygon to the map. Note that if there is no land in your region you'll receive a "nothing to draw" error.
-#' This can be fixed by using land=FALSE. Default is TRUE.
+#' @param colPalette  ggplot2 colour palette for map. Default is "Dark2".
 #' @return A map illustrating level 1 Infomap communities.
 #' @examples
 #' CommunityMap(infomap_object)
@@ -14,28 +12,39 @@
 #' CommunityMap(infomap_object, c(1,2,3))
 #' @export
 
-CommunityMap <- function(infomap_object, subset_communities, colPalette="Dark2", land=TRUE){
+CommunityMap <- function(infomap_object, subset_communities, colPalette="Dark2"){
 
   if (exists("infomap_object")==FALSE){
     stop("Please calculate Infomap communities using the InfomapCommunities function prior to executing CommunityMap")
   }
 
-  infomap_modules<-as.data.frame(infomap_object$modules)
+  infomap_modules <- as.data.frame(infomap_object$modules)
 
   if (!missing(subset_communities)){
     infomap_modules<-infomap_modules[which(infomap_modules$module_level1 %in% subset_communities),]
   }
 
-    xyz <- infomap_modules[,c("module_level1", "long", "lat")]
-    z <- ggplot2::ggplot() +
-      ggplot2::geom_tile(data=xyz, ggplot2::aes(x=long, y=lat, fill=as.factor(module_level1)))+
-      ggplot2::labs(x = "Longitude",y = "Latitude", fill = "Community")+
-      ggplot2::coord_sf(xlim = c(min(xyz$long), max(xyz$long)), ylim = c(min(xyz$lat), max(xyz$lat)))+
-      ggplot2::theme_minimal()+
-      ggplot2::scale_fill_brewer(palette=colPalette)
-    if (land==TRUE){
-      z <- z + ggplot2::borders("world", colour="gray50", fill="gray50", xlim = c(min(xyz$long), max(xyz$long)),
-                                ylim = c(min(xyz$lat), max(xyz$lat))) # create a layer of borders
-    }
-    plot(z)
+  if (length(unique(infomap_modules$module_level1)) > 8) { # ggplot pallets only have 8 unique colours but colorRampPalette can be used to extend the pallet
+    myColoursPal <- colorRampPalette(RColorBrewer::brewer.pal(8, colPalette))(length(unique(infomap_modules$module_level1)))
+  } else {
+    myColoursRaw <- colPalette
+  }
+
+  xyz <- infomap_modules[,c("module_level1", "long", "lat")]
+  z <- ggplot2::ggplot() +
+    ggplot2::geom_tile(data=xyz, ggplot2::aes(x=long, y=lat, fill=as.factor(module_level1)))+
+    ggplot2::labs(x = "Longitude",y = "Latitude", fill = "Community")+
+    ggplot2::coord_sf(xlim = c(min(xyz$long), max(xyz$long)), ylim = c(min(xyz$lat), max(xyz$lat)))+
+    ggplot2::theme_minimal()
+
+  if (exists("myColoursPal")==TRUE){ # If a new colour pallet was created
+    z <- z + ggplot2::scale_fill_manual(values = myColoursPal)
+  } else {
+    z <- z + ggplot2::scale_fill_brewer(palette = myColoursRaw) # If a standard ggplot2 colour pallet was used
+  }
+  tryCatch({
+    z <- z +
+      ggplot2::borders("world", colour="gray50", fill="gray50", xlim = c(min(xyz$long), max(xyz$long)), ylim = c(min(xyz$lat), max(xyz$lat)))
+  }, error = function(e){message('World polygon does not overlap with occupancy data')})
+  plot(z)
 }
