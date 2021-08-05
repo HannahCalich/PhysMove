f6e2b70b657af98979cdddb050aae9a38b776e34
Hannah <hannah.calich@research.uwa.edu.au>
2021-08-05 12:32:04 +0800

    Parameter, return, description text updates, spacing updates, ccdf data now in return instead of assign, since timeWindows and dist are no longer output from FitDist and CalcDisp this code was updated accordingly

--- R/PlotDist.R
+++ R/PlotDist.R
@@ -1,20 +1,21 @@
 #' Plot best-fit distributions to complementary cumulative distribution function (CCDF) of displacements
 #'
 #' This function allows you to plot a complementary cumulative distribution function (CCDF) of displacements with fit lines
-#' based on the distribution fits calculated with the \code{\link{FitDist}} function.
-#' @param displacements The output from the \code{\link{CalcDisp}} function.
+#' based on the displacements output from the \code{\link{CalcDisp}} function and the distribution fits calculated with the
+#' \code{\link{FitDist}} function.
+#' @param displacements List of displacements output from the \code{\link{CalcDisp}} function.
+#' @param distResults Data frame of results output from the \code{\link{FitDist}} function.
 #' @param fitLines Add fit lines based on the parameters calculated with the \code{\link{FitDist}} function. Default is TRUE.
-#' @param colours Colours for each fit line. Default is colours=c("red","gold2","blue").
-#' @param setDist Plot  a subset of the distribution fit lines calculated with the \code{\link{FitDist}} function. To plot a specific
-#' distribution fit line use setDist="pl", or to plot multiple distributions use setDist=c("pl","exp"). Options include "pl", "exp", and "lnorm".
-#' Default is NULL
+#' @param colours Colours for each fit line. Valid input options include colour names or hex numbers. Default is colours=c("red","gold2","blue").
+#' @param setDist Plot a subset of lines for each distribution fit calculated with the \code{\link{FitDist}} function (e.g., setDist=c("pl","exp"))
+#' Options include "pl", "exp", and "lnorm". By default all fit lines are plotted. Default is NULL
 #' @param legend Add a legend to the plot when TRUE and change the position of the legend. Default is legend=c(TRUE, "bottomleft")
 #' @return Complementary cumulative distribution function (CCDF) plot of displacements with fit lines (if fitLines=TRUE).
-#' @examples PlotDist(displacements)
-#' @examples PlotDist(displacements, fitLines=TRUE, colours=c("red","gold2","blue"), setDist=FALSE, legend=c(TRUE, "bottomleft"))
+#' @examples PlotDist(displacements,distResults)
+#' @examples PlotDist(displacements, distResults, fitLines=TRUE, colours=c("red","gold2","blue"), setDist=FALSE, legend=c(TRUE, "bottomleft"))
 #' @export
 
-PlotDist <- function (displacements, fitLines=TRUE, colours=c("red","gold2","blue"), setDist=NULL, legend=c(TRUE, "bottomleft")){
+PlotDist <- function(displacements, distResults, fitLines=TRUE, colours=c("red","gold2","blue"), setDist=NULL, legend=c(TRUE, "bottomleft")){
 
   if (exists("displacements")==FALSE){
     stop("Please calculate displacements using CalcDisp and fit distriubtions using FitDisp prior to executing PlotDist")
@@ -25,12 +26,12 @@ PlotDist <- function (displacements, fitLines=TRUE, colours=c("red","gold2","blu
   }
 
   if (is.null(setDist)){
-    setDist=dist
+    setDist <- distResults$distribution # Use all distributions used in FitDist
   }
 
   if (normalize){
     x <- list()
-    for (d in 1:length(timeWindows)){
+    for (d in 1:length(displacements)){
       disp <- unlist(displacements[d])
       x[[d]] <- disp/mean(disp)
     }
@@ -38,24 +39,22 @@ PlotDist <- function (displacements, fitLines=TRUE, colours=c("red","gold2","blu
     xlabel <- "Normalized displacements"
   } else {
     x <- unlist(displacements)
-    xlabel<-"displacements (km)"
+    xlabel <- "displacements (km)"
   }
 
   x <- sort(x)
   n <- length(x)
   ccdf <- 1-((0:(n - 1))/n)
   df <- data.frame(x=sort(x), y=ccdf)
-  plot(df, log="xy", ylab="CCDF", xlab=xlabel, xaxt="n", yaxt="n", col="black") #works same as powerlaw
-  myTicks = axTicks(1)
-  axis(1, at = myTicks, labels = formatC(myTicks, digits = 0, format = 'e'))
-  myTicks2 = axTicks(2)
-  axis(2, at = myTicks2, labels = formatC(myTicks2, digits = 0, format = 'e'))
-
-  names(df)=c(xlabel,"CCDF")
-  assign("CCDFplotData", df, envir = .GlobalEnv)
+  plot(df, log="xy", ylab="CCDF", xlab=xlabel, xaxt="n", yaxt="n", col="black")
+  myTicks <- axTicks(1)
+  axis(1, at=myTicks, labels=formatC(myTicks, digits=0, format='e'))
+  myTicks2 <- axTicks(2)
+  axis(2, at=myTicks2, labels=formatC(myTicks2, digits=0, format='e'))
+  names(df) <- c(xlabel,"CCDF")
 
   if (fitLines==TRUE){
-    legendcols<-c()
+    legendcols <- c()
     if ("pl" %in% setDist){
       MyPowerLawCDF <- function(parameters, displacements){
         PL_CDF  = 1 - (displacements/parameters[2])^(-parameters[1]+1)
@@ -63,21 +62,21 @@ PlotDist <- function (displacements, fitLines=TRUE, colours=c("red","gold2","blu
       }
       PL_xmin <- distResults[which(distResults$distribution=="pl"),"xmin"]
       PL_alpha <- distResults[which(distResults$distribution=="pl"),"parameter1"]
-      xval<-exp(seq(log(PL_xmin), log(max(x)), length.out = 100)) #log spaced sequence of displacements for log-log plot, matches poweRlaw x val
+      xval <- exp(seq(log(PL_xmin), log(max(x)), length.out = 100)) # log spaced sequence of displacements for log-log plot
       yval <- 1- MyPowerLawCDF(c(PL_alpha, PL_xmin), xval)
       yval[xval < round(PL_xmin)] = 0
-      dif = x - PL_xmin #PLAGARIZED FROM HERE TO SCALE STEP
-      upper = which(dif >= 0)[1]
-      lower = max(upper - 1, 1)
-      x_dif = x[lower] - x[upper]
-      y_dif = ccdf[lower] - ccdf[upper]
-      scale = ccdf[lower] + y_dif * (PL_xmin - x[lower])/x_dif
+      dif <- x - PL_xmin
+      upper <- which(dif >= 0)[1]
+      lower <- max(upper - 1, 1)
+      x_dif <- x[lower] - x[upper]
+      y_dif <- ccdf[lower] - ccdf[upper]
+      scale <- ccdf[lower] + y_dif * (PL_xmin - x[lower])/x_dif
       if (is.nan(scale)){
-        scale = 1
+        scale <- 1
         }
-      yval = yval * scale
+      yval <- yval * scale
       lines(xval, yval, col=colours[1],lwd=2)
-      legendcols<-c(legendcols, colours[1])
+      legendcols <- c(legendcols, colours[1])
     }
     if ("exp" %in% setDist){
       MyExponentialPDF<- function(parameters, displacements){
@@ -87,21 +86,21 @@ PlotDist <- function (displacements, fitLines=TRUE, colours=c("red","gold2","blu
       }
       Exp_xmin <- distResults[which(distResults$distribution=="exp"),"xmin"]
       Exp_lambda <- distResults[which(distResults$distribution=="exp"),"parameter1"]
-      xval<-exp(seq(log(Exp_xmin), log(max(x)), length.out = 100)) #log spaced sequence of displacements for log-log plot, matches poweRlaw x val
-      yval <- MyExponentialPDF(c(Exp_lambda, Exp_xmin), xval) # This works rounded, might not need to change
+      xval <- exp(seq(log(Exp_xmin), log(max(x)), length.out = 100)) # log spaced sequence of displacements for log-log plot
+      yval <- MyExponentialPDF(c(Exp_lambda, Exp_xmin), xval)
       yval[xval < Exp_xmin] = 0
-      dif = x - Exp_xmin #PLAGARIZED FROM HERE TO SCALE STEP
-      upper = which(dif >= 0)[1]
-      lower = max(upper - 1, 1)
-      x_dif = x[lower] - x[upper]
-      y_dif = ccdf[lower] - ccdf[upper]
-      scale = ccdf[lower] + y_dif * (Exp_xmin - x[lower])/x_dif
+      dif <- x - Exp_xmin
+      upper <- which(dif >= 0)[1]
+      lower <- max(upper - 1, 1)
+      x_dif <- x[lower] - x[upper]
+      y_dif <- ccdf[lower] - ccdf[upper]
+      scale <- ccdf[lower] + y_dif * (Exp_xmin - x[lower])/x_dif
       if (is.nan(scale)){
-        scale = 1
+        scale <- 1
       }
-      yval = yval * scale
+      yval <- yval * scale
       lines(xval, yval, col=colours[2],lwd=2)
-      legendcols<-c(legendcols, colours[2])
+      legendcols <- c(legendcols, colours[2])
     }
     if ("lnorm" %in% setDist){
       MyLogNormalPDF <- function(parameters, displacements){ # 1=mu, 2= sigma
@@ -112,25 +111,26 @@ PlotDist <- function (displacements, fitLines=TRUE, colours=c("red","gold2","blu
       LN_xmin <- distResults[which(distResults$distribution=="lnorm"),"xmin"]
       LN_mu <- distResults[which(distResults$distribution=="lnorm"),"parameter1"]
       LN_sigma <- distResults[which(distResults$distribution=="lnorm"),"parameter2"]
-      xval<-exp(seq(log(LN_xmin), log(max(x)), length.out = 100)) #log spaced sequence of displacements for log-log plot, matches poweRlaw x val
+      xval <- exp(seq(log(LN_xmin), log(max(x)), length.out = 100)) # log spaced sequence of displacements for log-log plot
       yval <- MyLogNormalPDF(c(LN_mu, LN_sigma, LN_xmin), xval)
       yval[xval < LN_xmin] = 0
-      dif = x - LN_xmin #PLAGARIZED FROM HERE TO SCALE STEP
-      upper = which(dif >= 0)[1]
-      lower = max(upper - 1, 1)
-      x_dif = x[lower] - x[upper]
-      y_dif = ccdf[lower] - ccdf[upper]
-      scale = ccdf[lower] + y_dif * (LN_xmin - x[lower])/x_dif
+      dif <- x - LN_xmin
+      upper <- which(dif >= 0)[1]
+      lower <- max(upper - 1, 1)
+      x_dif <- x[lower] - x[upper]
+      y_dif <- ccdf[lower] - ccdf[upper]
+      scale <- ccdf[lower] + y_dif * (LN_xmin - x[lower])/x_dif
       if (is.nan(scale)){
-        scale = 1
+        scale <- 1
       }
-      yval = yval * scale
+      yval <- yval * scale
       lines(xval, yval, col=colours[3],lwd=2)
-      legendcols<-c(legendcols, colours[3])
+      legendcols <- c(legendcols, colours[3])
       }
     if (legend[1]==TRUE){
         legend(legend[2], legend=setDist, lwd=1, col=legendcols, bty="n", y.intersp=0.75)
     }
   }
+  return(df)
 }
 
