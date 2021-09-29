132ad105ca8884cc6a34d95899fe34403f5b1fa2
Hannah <hannah.calich@research.uwa.edu.au>
2021-09-29 09:05:03 +0800

    Updated to aus spelling to match manuscript

--- R/PlotRandomTracks.R
+++ R/PlotRandomTracks.R
@@ -1,22 +1,22 @@
-#' Plot Randomized Tracks
+#' Plot Randomised Tracks
 #'
-#' Plot locations from original and reshuffled tracks using randomizedLat and randomizedLong outputs from the \code{\link{Randomize}} function
+#' Plot locations from original and reshuffled tracks using RandomisedLat and RandomisedLong outputs from the \code{\link{Randomise}} function
 #' @param species_df A data frame containing location data in rows. Columns have the following headers: "ref", "lon", "lat", "day".
 #' "ref" is the unique id number for each animal (e.g., their satellite tag number formatted as an integer),
 #' "lon" and "lat" are the longitude and latitude of each position estimate in decimal degrees in numeric format),
 #' "day" is the datetime stamp for each location estimate in POSIXct format following yyyy-mm-dd hh:mm:ss.
 #' See attached sample data \code{\link{speciesA}}.
 #' @param ref Reference number of track from species_df to plot.
-#' @param numPlot Number of randomized tracks to plot. The randomized tracks were consecutively numbered from 1 to however many you set in the
-#' \code{\link{Randomize}} function. The input value can either be any of these individual numbers (e.g., 23), or a range of numbers (e.g., 1:10),
+#' @param numPlot Number of Randomised tracks to plot. The Randomised tracks were consecutively numbered from 1 to however many you set in the
+#' \code{\link{Randomise}} function. The input value can either be any of these individual numbers (e.g., 23), or a range of numbers (e.g., 1:10),
 #' which will plot all of the random tracks created within the range. Default is 1:5.
-#' @param colours Colours to plot points from original and randomized tracks, respectively. Default is colours=c("black","grey70").
+#' @param colours Colours to plot points from original and Randomised tracks, respectively. Default is colours=c("black","grey70").
 #' @param tracks Add track lines to the plot. Default is TRUE.
 #' @param startCol Colour for origin location. startCol=NULL will cause the symbology of the origin location to match the symbology of the rest
 #' of the original track. Default is startCol="red".
 #' @param endCol Colour for destination location. endCol=NULL will cause the symbology of the destination location to match the symbology of the rest
 #' of the original track. Default is endCol="blue".
-#' @return Plot showing the original and randomized track locations and the randomized tracks data used to create the map (original tracks are
+#' @return Plot showing the original and Randomised track locations and the Randomised tracks data used to create the map (original tracks are
 #' from species_df).
 #' @examples PlotRandomTracks<-function(speciesA, ref=1)
 #' @examples PlotRandomTracks<-(speciesA, ref=1, numPlot=1:5, colours=c("black","grey70"), tracks=TRUE, startCol="red", endCol="blue")
@@ -25,8 +25,8 @@
 PlotRandomTracks<-function(species_df, ref=NULL, numPlot=1:5, colours=c("black","grey70"),
                            tracks=TRUE, startCol="red", endCol="blue"){
 
-  if ((exists("randomizedLat")& exists("randomizedLong")& exists("randTrack"))==FALSE){
-    stop("Please create randomized tracks using the Randomize function prior to executing PlotRandomTracks")
+  if ((exists("RandomisedLat")& exists("RandomisedLong")& exists("randTrack"))==FALSE){
+    stop("Please create Randomised tracks using the Randomise function prior to executing PlotRandomTracks")
   }
 
   if(is.null(ref)==TRUE || !(ref %in% species_df$ref)){
@@ -34,8 +34,8 @@ PlotRandomTracks<-function(species_df, ref=NULL, numPlot=1:5, colours=c("black",
   }
 
   if (length(numPlot)>randTrack){
-    stop("You cannot plot more random tracks than you created with the Randomize function. Please adjust the numPlot value or
-         re-run Randomize to create more tracks")
+    stop("You cannot plot more random tracks than you created with the Randomise function. Please adjust the numPlot value or
+         re-run Randomise to create more tracks")
   }
 
   species_index <- tapply(1:nrow(species_df), species_df[,1], function(x){x})
@@ -43,14 +43,14 @@ PlotRandomTracks<-function(species_df, ref=NULL, numPlot=1:5, colours=c("black",
   a <-  as.character(unique(Individual$ref))
   plotpoints <- data.frame("lat"=integer(0),"lon"=integer(0)) # Format the df, the 0s are removed
 
-  for(T in numPlot){ #For the randomized data
-    lat_long <- data.frame("RandomTraj"=T, "lon"=randomizedLong[species_index[[a]],T],"lat"=randomizedLat[species_index[[a]],T])
+  for(T in numPlot){ #For the Randomised data
+    lat_long <- data.frame("RandomTraj"=T, "lon"=RandomisedLong[species_index[[a]],T],"lat"=RandomisedLat[species_index[[a]],T])
     plotpoints <- rbind(plotpoints,lat_long)
   }
 
   Individual <- cbind(Track=rep("Original",nrow(Individual)),Individual)
   Individual <- Individual[,1:4]
-  plotpoints <- cbind(Track=rep("Randomized",nrow(plotpoints)),plotpoints)
+  plotpoints <- cbind(Track=rep("Randomised",nrow(plotpoints)),plotpoints)
   names(plotpoints) <- names(Individual) # make sure col names match
   plot.df <- rbind(plotpoints,Individual)
   plot.df <- cbind(plot.df, "trackRef"=paste(plot.df$Track,plot.df$ref,sep="_"))
@@ -68,7 +68,7 @@ PlotRandomTracks<-function(species_df, ref=NULL, numPlot=1:5, colours=c("black",
 
   if (tracks==TRUE){
     a <- a +
-      ggplot2::geom_path(data = subset(plot.df, Track == 'Randomized'))+
+      ggplot2::geom_path(data = subset(plot.df, Track == 'Randomised'))+
       ggplot2::geom_path(data = subset(plot.df, Track == 'Original'))+
       ggplot2::geom_point(data = subset(plot.df, Track == 'Original'))
   }
