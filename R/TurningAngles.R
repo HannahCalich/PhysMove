37fd809b2032f07ec7e76969327f4600720e16c9
Hannah <hannah.calich@research.uwa.edu.au>
2021-10-08 14:35:39 +0800

    base size and x labels updated

--- R/TurningAngles.R
+++ R/TurningAngles.R
@@ -142,12 +142,12 @@ TurningAngles<-function(species_df, min_hr=24, max_hr=240, interval_hr=24, range
       names(angles.df) <- "Angles"
       h <- hist(angles.df$Angles, plot = FALSE, breaks = seq(-180, 180, bins)) # Plot all angels for selected time periods from all individuals
     }
-    xlabels <- c("", "-160", "", "-120", "", "-80", "", "-40", "", "0", "", "40", "", "80", "", "120","", "160", "")
+    xlabels <- c("-180", "", "-120",  "",  "-60",  "",  "0", "",  "60", "", "120",  "",  "180")
     hist_plot <- ggplot2::ggplot(angles.df, ggplot2::aes(Angles))+
       ggplot2::geom_histogram(breaks=h$breaks, color="black", fill="darkgrey")+
-      ggplot2::scale_x_continuous("Turning Angles", breaks=seq(-180,180,20), labels=xlabels)+
-      ggplot2::labs(y = "Frequency")+
-      ggplot2::theme_classic()
+      ggplot2::scale_x_continuous("Turning Angles", breaks=seq(-180,180,30), labels=xlabels)+
+      ggplot2::labs(y="Frequency")+
+      ggplot2::theme_classic(base_size=18)
     plot(hist_plot)
   }
   return(angleList)
