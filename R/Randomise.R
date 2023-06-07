ff1e3613b2ecce6533d3c68c04827b9ebaf29ac5
HannahCalich <73092681+HannahCalich@users.noreply.github.com>
2023-06-07 16:42:39 +0800

    Cleaned up comments and removed references to time meaurments that are not curently being used

--- R/Randomise.R
+++ R/Randomise.R
@@ -1,11 +1,11 @@
 #' Randomise tracks
 #'
-#' This function allows you to investigate the influence spatial and/or temporal correlations may have on an indiivudals' space use patterns.
+#' This function allows you to investigate the influence spatial and/or temporal correlations may have on an individuals' space use patterns.
 #' This is done by maintaining the origin and end location of each track, randomizing the order displacements occurred in between these points,
-#' and calculating how many grid cells the original and Randomised tracks visited, which summarizes their space use.
+#' and calculating how many grid cells the original and randomised tracks visited, which summarizes their space use.
 #' @param species_df A data frame containing location data in rows. Columns have the following headers: "ref", "lon", "lat", "day".
 #' "ref" is the unique id number for each animal (e.g., their satellite tag number formatted as an integer),
-#' "lon" and "lat" are the longitude and latitude of each position estimate in decimal degrees in numeric format),
+#' "lon" and "lat" are the longitude and latitude of each position estimate in decimal degrees in numeric format,
 #' "day" is the datetime stamp for each location estimate in POSIXct format following yyyy-mm-dd hh:mm:ss.
 #' See attached sample data \code{\link{tracks}}.
 #' @param randTrack Number of randomised tracks per individual. Default is 100.
@@ -22,22 +22,18 @@
 #' Randomised tracks for each ref. Lastly, if plot = TRUE, a plot illustrating the number of cells visited by the original and randomised tracks is created,
 #' and if lm = TRUE, a fit line and reference line are added to the plot.
 #' @examples
-#' Randomise(tracks)
-#' Randomise(tracks, randTrack=500, gridCell=0.25, plot=TRUE, lm=TRUE)
+#' Randomise(tracks, randTrack=100, gridCell=0.25, plot=TRUE, lm=TRUE)
 #' @export
 
-Randomise<-function(species_df, randTrack=100, gridCell=0.25, plot=TRUE, lm=TRUE) {
+Randomise <- function(species_df, randTrack=100, gridCell=0.25, plot=TRUE, lm=TRUE) {
 
   species_index <- tapply(1:nrow(species_df), species_df[,1], function(x){x})
-  MyDiffLat <- MyDiffLong <- MyDiffTime <- rep(0, dim(species_df)[1]) # Create vector to store the differences in lat, long and time (time is needed for data that are not interpolated)
+  MyDiffLat <- MyDiffLong <- MyDiffTime <- rep(0, dim(species_df)[1]) # Create vector to store the differences in lat and long
   for (i in 2:dim(species_df)[1]){
     # For all rows starting at 2, subtract the difference between row i and the row before it.
     # Includes differences between individuals but those are called on in next section of code
-    # MyDiff[i] = diff from row before it to current row
-    # if(species_df[i,1] == species_df[i-1,1]){ # no need for this as long as we don't read values calculated at the index for origin for each individuals
     MyDiffLong[i] <- species_df[i,2] - species_df[i-1,2]
     MyDiffLat[i] <- species_df[i,3] - species_df[i-1,3]
-    # MyDiffTime[i] <- as.numeric(difftime(species_df[i,4], species_df[i-1,4], units="hours")) #diff time in hours
   }
   # Shuffle the differences per individual using sample without replacement
   ShuffledLong <- ShuffledLat <- ShuffledTime <- matrix(0, nrow = dim(species_df)[1], ncol = randTrack) # Create vector to store the shuffled differences per individual
@@ -51,24 +47,17 @@ Randomise<-function(species_df, randTrack=100, gridCell=0.25, plot=TRUE, lm=TRUE
       # For each reshuffling (T), reserve the original first location per individual (i).
       ShuffledLong[species_index[[i]][1], T] <- species_df[species_index[[i]][1], 2] # first position for each individual is the origin
       ShuffledLat[species_index[[i]][1], T] <- species_df[species_index[[i]][1], 3]
-      # ShuffledTime[species_index[[i]][1], T] <- species_df[species_index[[i]][1], 4]
-      # ShuffledTime[species_index[[i]][1], T] <- 0 #time at first location is 0
-      # ShuffledTime[species_index[[i]][1], T] <- as.character(species_df[species_index[[i]][1], 4]) # R wont store datetime objects in matrices, work around is to save as.character then convert to as.date for calculations
       for(j in 1:length(NewPositions)){
         # Looping through new positions, length is 1 fewer than the number of locations per animal because first position is fixed
-        # Create random locations based on the sum of previous location and the distance traveled between a random location and the next point in the track (MyDiff)
-        # If the first new position is 26, we want to add the difference between the previous location (the origin), and the myDiff[27],
+        # Create random locations based on the sum of previous location and the distance travelled between a random location and the next point in the track (MyDiff)
+        # If the first new position is 26, we want to add the difference between the previous location (the origin), and the MyDiff[27],
         # which is the distance from the 26th to 27th points, moving forwards towards the end of the track.
-        # Since NewPositions is n-1, working backwards with the MyDiffs and adding 1 here makes sure the last MyDiff per species is included
+        # Since NewPositions is n-1, working backwards with the MyDiff and adding 1 here makes sure the last MyDiff per species is included
         # (eg if an individual has 65 points, there are 64 new ones, but the code below will make sure the distance between the 65th and 64th point is included)
         # Since this loop is limited by length(NewPositions), we won't include the diffs calculated between individuals
-        # This way, the math results in the same final destination point for each track
-        # Double checked this part of the code with the raw data to confirm the origin and destination points match (2019-11-07)
+        # Following this approach the math results in the same final destination point for each track
         ShuffledLong[species_index[[i]][j+1], T] <- ShuffledLong[species_index[[i]][j], T] + MyDiffLong[species_index[[i]][NewPositions[j]+1]]
         ShuffledLat[species_index[[i]][j+1], T] <- ShuffledLat[species_index[[i]][j], T] + MyDiffLat[species_index[[i]][NewPositions[j]+1]]
-        # ShuffledTime[species_index[[i]][j+1], T] <- ShuffledTime[species_index[[i]][j], T] + MyDiffTime[species_index[[i]][NewPositions[j]+1]]
-        # ShuffledTime[species_index[[i]][j+1], T] <- as.character(as.POSIXct(ShuffledTime[species_index[[i]][j], T]) + as.difftime(as.numeric(MyDiffTime[species_index[[i]][NewPositions[j]+1]]), units="hours"))
-        # ShuffledTime[species_index[[i]][j+1], T] <- as.numeric(MyDiffTime[species_index[[i]][NewPositions[j]+1]])
       }
     }
   }
@@ -77,21 +66,21 @@ Randomise<-function(species_df, randTrack=100, gridCell=0.25, plot=TRUE, lm=TRUE
 
   # Compare the number of visited cells used in the original and reshuffled tracks
   grid <- 1/gridCell
-  longmin <- -180 # min(species_df[species_index[[1]],2])
-  latmin <- -90 # min(species_df[species_index[[1]],3])
-  longmax <- 180 # max(species_df[species_index[[1]],2])
-  latmax <- 90 # max(species_df[species_index[[1]],3])
-  longcells <- grid * (longmax - longmin) # 360*4 = 1440 cells of 0.25 degrees each in longitude
-  latcells <- grid * (latmax - latmin)  # 180*4 = 720 cells of 0.25 degrees each in latitude
-  totalcells <- longcells * latcells # 1036800 for 0.25 deg res
+  longmin <- -180
+  latmin <- -90
+  longmax <- 180
+  latmax <- 90
+  longcells <- grid * (longmax - longmin)
+  latcells <- grid * (latmax - latmin)
+  totalcells <- longcells * latcells
 
   SumShuffledOccurrences <- matrix(0, nrow = length(species_index), ncol = randTrack)
   AvgShuffledOccurrences <- SumOriginalOccurrences <- c() # vector to store cell counts for each individual
 
   # Loop through each shuffled position and store counts when individuals occur in each cell
   for (i in 1:length(species_index)){ # For each individual
-    for(T in 1:randTrack){ # For each random track, determine what cell each point falls in){
-      j <- 1 # Do not turn this line off (it marks the position for each index per individual)
+    for(T in 1:randTrack){ # For each random track, determine what cell each point falls in
+      j <- 1 # Do not comment this line because it marks the position for each index per individual
       ShuffledPresence <- rep(0, totalcells)
       for (j in 1:length(species_index[[i]])){ # For each point in a track
         coordlong <- floor(grid * (ShuffledLong[species_index[[i]][j], T] - longmin))
@@ -106,25 +95,14 @@ Randomise<-function(species_df, randTrack=100, gridCell=0.25, plot=TRUE, lm=TRUE
 
   message("Calculating number of cells visited by original tracks, step 3/3")
 
-  # split time diff between each location
-  # CellTime <- tapply(1:nrow(species_df), species_df[,1], function(x){x})
-  # for (i in 1:length(species_index)){ # For each individual
-    # CellTime[[i]][1] <- 0
-    # for (j in 2:length(species_index[[i]])){
-      # CellTime[[i]][j] <- as.numeric(difftime(species_df[species_index[[i]][j],4],species_df[species_index[[i]][j-1],4], units="hours"))/2 # time betwen point j and the previous point
-    # }
-  # }
-
   for (i in 1:length(species_index)){ # For original tracks
     j < -1 # Do not comment this. This is needed to restart j for each animal to find position in index.
     Presence <- rep(0, totalcells)
-    # tdiff <- rep(0,totalcells)
     for (j in 1:length(species_index[[i]])){
       coordlong <- floor(grid * (as.numeric(species_df[species_index[[i]][j],2]) - longmin))
       coordlat <- floor(grid * (as.numeric(species_df[species_index[[i]][j],3]) - latmin))
       cellnum <- coordlong + grid * (longmax - longmin) * coordlat
       Presence[cellnum] <- 1 # Record the number of unique cells visited (aka presence, not occupancy)
-      # tdiff[cellnum] <- tdiff[cellnum] + CellTime[[i]][j]
     }
     SumOriginalOccurrences[i] <- sum(Presence)
   }
