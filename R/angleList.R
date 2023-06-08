9f71227013a0ae02b7a4b88a820bbbd76eee0bfb
HannahCalich <73092681+HannahCalich@users.noreply.github.com>
2023-06-08 20:54:30 +0800

    Updates to sample data

--- R/angleList.R
+++ R/angleList.R
@@ -1,7 +1,7 @@
 #' Example output from TurningAngles to demonstrate the PlotAngles function
 #'
-#' Output from \code{\link{TurningAngles}} function calculated using the tracks dataset and \code{\link{TurningAngles}} with max_hr = 120. Note that the TurningAngles
-#' default is normally max_hr=240; however, this creates an unnecessarily large file for an example dataset so max_hr = 120 was used instead.
+#' Output from \code{\link{TurningAngles}} function calculated using the tracks dataset and \code{\link{TurningAngles}} with max_hr = 24. Note that the TurningAngles
+#' default is normally max_hr=240; however, this creates an unnecessarily large file for an example dataset so max_hr = 24 was used instead.
 #'
-#' @format List containing 10 list elements, 1 for each time window set using \code{\link{TurningAngles}} default settings
+#' @format List containing 10 list elements, 1 for each time window set using \code{\link{TurningAngles}}
 "angleList"
--- R/disp.R
+++ R/disp.R
@@ -1,6 +1,6 @@
 #' Sample displacement data to demonstrate PhysMove functions
 #'
-#' A list containing the displacements in km recorded for each time window using CalcDisp default parameters
+#' A list containing the displacements in km recorded for each time window using CalcDisp with max_hr=24.
 #'
 #' @format A list with 10 elements
 "disp"
