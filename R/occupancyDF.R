4f899645647497db56cab7d50a00e76e648af363
HannahCalich <73092681+HannahCalich@users.noreply.github.com>
2023-06-08 13:24:56 +0800

    example data updates

--- R/occupancyDF.R
+++
@@ -1,7 +0,0 @@
-#' Example output from Occupancy function for pdfPlot function
-#'
-#' Re-formatted results from Occupancy function using default parameters. Occupancy output is normally in list format but it was converted to a data frame for
-#' simplicity with pdfPlot example (i.e., occupancy.df <- as.data.frame(Occ[[1]]), where Occ was the output from the Occupancy function.
-#'
-#' @format data frame
-"occupancyDF"
--- R/randomResults.R
+++ R/randomResults.R
@@ -1,6 +1,7 @@
 #' Example output from Randomise to demonstrate the PlotRandomTracks function
 #'
-#' Output from \code{\link{Randomise}} function calculated using the \code{\link{tracks}} dataset and default \code{\link{Randomise}} parameters
+#' Output from \code{\link{Randomise}} function calculated using the \code{\link{tracks}} dataset and \code{\link{Randomise}} with randTrack=10. Note that the default is
+#' normally randTrack=100; however, this creates an unnecessarily large file for an example dataset so randtrack=10 was used instead.
 #'
-#' @format List containing 4 list elements, based on \code{\link{Randomise}} default parameters
+#' @format List containing 4 list elements, based on \code{\link{Randomise}}
 "randomResults"
