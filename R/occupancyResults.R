16ffaa3648c13a3d4c49ef39467493ff9f46a433
HannahCalich <73092681+HannahCalich@users.noreply.github.com>
2023-06-08 14:26:17 +0800

    Example data updates

---
+++ R/occupancyResults.R
@@ -0,0 +1,6 @@
+#' Example output from Occupancy function for pdfPlot function
+#'
+#' Results from Occupancy function using Occupancy() default parameters.
+#'
+#' @format data frame
+"occupancyResults"
