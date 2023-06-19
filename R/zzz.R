e5e54fb7d52b820011c6dd61c828b0a089e4e8b7
HannahCalich <73092681+HannahCalich@users.noreply.github.com>
2023-06-19 20:04:24 +1000

    Add .onLoad and .onAttach for infomap

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+
+.onLoad  <- function(libname, pkgname) {
+  infomap_req <- requireNamespace("infomapecology", quietly = TRUE)
+  .pkgenv[["infomap_req"]] <- infomap_req
+}
+
+.onAttach <- function(libname, pkgname) {
+  if (!.pkgenv$infomap_req) {
+    msg <- paste("To use this package in conjunction with Infomap,",
+                 "you must install the infomapecology R package and the standalone Infomap file.",
+                 "See the `PhysMove: Space-Use Patterns` vignette for further details.")
+    msg <- paste(strwrap(msg), collapse="\n")
+    packageStartupMessage(msg)
+  }
+}
