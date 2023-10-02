0e0d71b2b8d641a2a31f10aa97c7bc2e74127660
HannahCalich <73092681+HannahCalich@users.noreply.github.com>
2023-10-02 10:36:29 +0800

    Spell check

--- R/tracks.R
+++ R/tracks.R
@@ -6,7 +6,7 @@
 #' \describe{
 #'   \item{ref}{trajectory's unique id number as an integer}
 #'   \item{lon}{longitude of each position estimate in decimal degrees}
-#'   \item{lat}{latitide of each position estimate in decimal degrees}
+#'   \item{lat}{latitude of each position estimate in decimal degrees}
 #'   \item{day}{datetime stamp for each location estimate in POSIXct format following: yyyy-mm-dd hh:mm:ss}
 #'   ...
 #' }
