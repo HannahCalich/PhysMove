ebf0f42921760e31d18cd8dcbfa6488ca98ae4a4
HannahCalich <73092681+HannahCalich@users.noreply.github.com>
2024-07-31 22:58:04 +0800

    rename disp to input to clarify that input values do not have to be displacements

--- R/input.R
+++ R/input.R
@@ -1,7 +1,7 @@
 #' Example output from CalcDisp()
 #'
-#' Example output from the \code{\link{CalcDisp}} function. Displacements in km calculated using the
-#' \code{\link{tracks}} dataset and CalcDisp() with max_hr=24. Note that the CalcDisp() default is
+#' Example output from the \code{\link{calcDisp}} function. Displacements in km calculated using the
+#' \code{\link{tracks}} dataset and calcDisp() with max_hr=24. Note that the calcDisp() default is
 #' normally max_hr=240; however, this creates an unnecessarily large file for an example dataset
 #' so max_hr=24 was used instead.
 #'
