308d135bf06b12e5503bc15ea9298b6009520054
HannahCalich <73092681+HannahCalich@users.noreply.github.com>
2025-02-26 17:38:20 +0800

    renamed functions following lowercaseUppercase nameing convention

--- doc/CreateExampleData.R
+++ doc/CreateExampleData.R
@@ -1,34 +1,34 @@
 ## create example data
 
-setwd("~2023/PhysMove/data")
+setwd("~2025/PhysMove/data")
 
 library(PhysMove)
 
-angleList <- TurningAngles(tracks, max_hr = 24)
+angleList <- turningAngles(tracks, max_hr = 24)
 save(angleList, file ="angleList.RData")
 
-disp <- CalcDisp(tracks, max_hr=24)
+disp <- calcDisp(tracks, max_hr=24)
 save(disp, file="disp.RData")
 
-distResultsAll <- FitDist(disp, full=TRUE)
+distResultsAll <- fitDist(disp, full=TRUE)
 save(distResultsAll, file="distResultsAll.RData")
 
-distResultsTrunc <- FitDist(disp, full=FALSE) # find best-fit dmin for all
-distResultsExp <- FitDist(disp, set_dmin = distResultsTrunc[[1]][2,2], normalise = distResultsTrunc[2][[1]]) # fit all dist to best fit dmin for exp
+distResultsTrunc <- fitDist(disp, full=FALSE) # find best-fit dmin for all
+distResultsExp <- fitDist(disp, set_xmin = distResultsTrunc[[1]][2,2], normalise = distResultsTrunc[2][[1]]) # fit all dist to best fit dmin for exp
 save(distResultsExp, file="distResultsExp.RData")
 
-entropyResults <- Entropy(tracks)
+entropyResults <- entropy(tracks)
 save(entropyResults, file="entropyResults.RData")
 
-setwd("~/2023/PhysMove")
+setwd("~/2025/PhysMove")
 library(infomapecology)
-infomapResult <- InfomapCommunities(tracks, tpm=TRUE)
-setwd("~/2023/PhysMove/PhysMove_Git/PhysMove/data")
+infomapResult <- infomapCommunities(tracks, tpm=TRUE)
+setwd("~/2025/PhysMove/PhysMove_Git/PhysMove/data")
 save(infomapResult, file="infomapResult.RData")
 
-occupancyResults <- Occupancy(tracks)
+occupancyResults <- occupancy(tracks)
 save(occupancyResults, file="occupancyResults.RData")
 
-randomResults <- Randomise(tracks, randTrack = 1)
+randomResults <- randomise(tracks, randTrack = 1)
 save(randomResults, file="randomResults.RData")
 
