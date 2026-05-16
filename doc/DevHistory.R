b54012664aac9cb511635d2bc3e74f5f861ae385
Hannah Calich <hannah.calich@gmail.com>
2026-05-16 23:09:04 +0800

    Updated with current dev status as of May 16 2026

--- doc/DevHistory.R
+++ doc/DevHistory.R
@@ -1,114 +1,87 @@
 ### Package development and CRAN checks
 
 ### Dev history based on recommendations from:
-# https://www.mzes.uni-mannheim.de/socialsciencedatalab/article/r-package/
-# https://github.com/ThinkR-open/prepare-for-cran
+### https://www.mzes.uni-mannheim.de/socialsciencedatalab/article/r-package/
+### https://github.com/ThinkR-open/prepare-for-cran
 
 #===========================================================================================
 ### Package details
 #===========================================================================================
 devtools::document() # Update manual
 # usethis::use_news_md(open = rlang::is_interactive()) # Create NEWS - Bump version manually and add list of changes
-usethis::use_cran_comments(open = rlang::is_interactive()) # Update comments for CRAN
-#usethis::use_version("minor") # Upgrade version number, options include: c("patch", "minor", "major", "dev")
+# usethis::use_cran_comments(open = rlang::is_interactive()) # Update comments for CRAN
+# usethis::use_version("minor") # Upgrade version number, options include: c("patch", "minor", "major", "dev")
 # usethis::use_gpl_license() # Update license info - if applicable
 devtools::spell_check() # Check spelling
 # attachment::att_amend_desc(pkg_ignore = c("infomapecology"), extra.suggests = c("infomapecology"), update.config = TRUE) # Amend dependencies in description
 # Code above adds DEV folder which is non-standard and needs to be manually deleted
-
 checkhelper::find_missing_tags() # check for missing return value for exported functions and missing @export tags
-#all_files_remaining <- checkhelper::check_clean_userspace() # listed as "experimental" in cran docs so skipping this because it's giving unclear messages about temp files
-#all_files_remaining
-# checkhelper::check_as_cran()# listed as "experimental" in cran docs so skipping this because it's not checking the correct packages
 urlchecker::url_check()
-tools::checkVignettes()
+
+### RESULTS:
+### PASS
+
 #===========================================================================================
 ### Package standard formatting checks
 #===========================================================================================
 # goodpractice::gp() # Goodpractice check -- skipping for now, some good ideas but not essential
-inteRgrate::check_pkg() # "installs package dependencies, builds & installs the package, before running package check"
-inteRgrate::check_lintr() # check if code "adheres to standards" -- skipping this, some good ideas but not essential
-inteRgrate::check_tidy_description() # Check description is tidy -- OK
-inteRgrate::check_r_filenames() # Check file names are correct -- file names should be lower case but this makes things too hard to read
-inteRgrate::check_gitignore() # Check .gitignore contains standard files -- OK
+# inteRgrate::check_pkg() # "installs package dependencies, builds & installs the package, before running package check"
+# inteRgrate::check_lintr() # check if code "adheres to standards" -- skipping this, some good ideas but not essential
+# inteRgrate::check_tidy_description() # Check description is tidy -- OK
+# inteRgrate::check_r_filenames() # Check file names are correct -- file names should be lower case but this makes things too hard to read
+# inteRgrate::check_gitignore() # Check .gitignore contains standard files -- OK
 
-### Summary of standard formatting checks:
-## goodpractice::gp() and inteRgrate::check_lintr() has some good ideas but they aren't essential
-## File names should be converted to lower case but this makes the function names too hard to read so skipping this for now
-## All others ok/pass
+### RESULTS:
+### SKIPPED
 
 #===========================================================================================
 ### Run tests and examples
 #===========================================================================================
-# usethis::use_test("PhysMove") # set up test files (commented because this creates initial test files)
 devtools::test() # Runs all tests in package -- Pass
-devtools::test_coverage() # Determines the percentage of package covered by tests -- may need to restart r session for this to work
 devtools::run_examples() # Check examples -- OK
 
-### Summary of test and example checks:
-## test_coverage not used because all troubleshooting was done before I learned about this option, but it will be used in future
-## All others ok/pass
+### RESULTS:
+### PASS
 
 #===========================================================================================
 ### Check package as CRAN
 #===========================================================================================
-devtools::check() # Local R CMD check -- this is giving a nonsense error about doc file size being too large
+devtools::check() # Local R CMD check
 devtools::check(remote = TRUE, manual = TRUE) # Remote CRAN check with manual -- some expected notes, see below
-# rcmdcheck::rcmdcheck()
+rcmdcheck::rcmdcheck()
 rcmdcheck::rcmdcheck(args = c("--no-manual"), build_args = c("--no-manual")) # Removed manual as I don't have LaTeX installed locally
 
-
+### RESULTS:
+### 3 Notes --
+### checking installed package size (note added to cran comments)
+### checking for future file timestamps (likely d/t OneDrive)
+### Suggests or Enhances not in mainstream repositories (note added to cran comments)
 
 #===========================================================================================
 ### Version checks (>=4.4)
 #===========================================================================================
-
 rhub::rhub_check(platform = "linux", r_version = "4.4")
 rhub::rhub_check(platform = c("linux", "windows", "macos"))
 rhub::rhub_check(platform = "linux", r_version = "devel")
 
+### RESULTS:
+### 3x PASS
+
 #===========================================================================================
 ### Test drat repos work
+#===========================================================================================
 install.packages("infomapecology", repos="https://HannahCalich.github.io/drat")
 install.packages("emln", repos="https://HannahCalich.github.io/drat")
-#===========================================================================================
 
+### RESULTS:
+### 2x INSTALLED
+
+#===========================================================================================
+### Rhub action checks
+#===========================================================================================
 # rhub::rhub_setup() # done already
 # rhub::rhub_check() # moved to GitHub actions.
 # Github actions yaml files updated to ignore infomapecology and emln based on bug reported here: https://github.com/r-hub/rhub2/issues/11
-
-#===========================================================================================
-### Summary of devtools notes
-#===========================================================================================
-## Summary of CRAN checks incl on other distributions:
-## checking CRAN incoming feasibility ... NOTE
-## Maintainer: 'Hannah J. Calich <hannah.calich@anu.edu.au>'
-##
-## New submission
-##
-## Suggests or Enhances not in mainstream repositories:
-##   emln, infomapecology
-## Availability using Additional_repositories specification:
-##   emln             yes   https://HannahCalich.github.io/drat
-## infomapecology   yes   https://HannahCalich.github.io/drat
-##
-## Found the following (possibly) invalid URLs:
-##   URL: https://github.com/HannahCalich/PhysMove
-## From: DESCRIPTION
-## Status: 404
-## Message: Not Found
-## URL: https://github.com/HannahCalich/PhysMove/actions
-## From: README.md
-## Status: 404
-## Message: Not Found
-## URL: https://github.com/HannahCalich/PhysMove/issues
-## From: DESCRIPTION
-## Status: 404
-## Message: Not Found
-##
-## Size of tarball: 5422399 bytes
-##
-## 0 errors ✔ | 0 warnings ✔ | 1 note ✖
 
 #===========================================================================================
 ### Check reverse dependencies -- Not currently relevant but keeping for future
