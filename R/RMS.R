6641affa32e746c405a4be2e0414ce456a3577a2
Hannah Calich <hannah.calich@gmail.com>
2026-07-24 18:12:27 +0800

    Remove option to use time unit of weeks as this is not standard in ecology

--- R/RMS.R
+++ R/RMS.R
@@ -6,7 +6,7 @@
 #' "lon" and "lat" are the longitude and latitude of each position estimate in decimal degrees in numeric format,
 #' "day" is the datetime stamp for each location estimate in POSIXct format following '%Y-%m-%d %H:%M:%S'.
 #'  Data frame must also be sorted by ref and then day within each ref, see \code{\link{checkTracks}} for details.
-#' @param timeUnit Unit used to calculate time between locations (e.g., "secs", "mins", "hours", "days", "weeks"). Default is "days".
+#' @param timeUnit Unit used to calculate time between locations (e.g., "secs", "mins", "hours", "days"). Default is "days".
 #' @param wBins Bin width refers to the size of the time bins used to calculate how frequently displacements occurred. Default is 1.1
 #' @param plot Plot the root-mean-square and mean displacements against their corresponding time periods. Default is TRUE.
 #' @param lm Calculate a linear regression to examine the relationship between the root-mean-square displacement values (target variable)
